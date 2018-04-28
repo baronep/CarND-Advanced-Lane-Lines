1405c3e2ddf4874f6b9f6824a50618152f5fc662
Patrick Barone <barone.patrick@gmail.com>
2018-04-28 09:58:39 -0700

    Adding camera_calibration images

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -1,37 +1,58 @@
+"""
+This script will process all of the checkerboard images in the folder 'input_image_path' and 
+generate a camera calibration as well as annotated output images
+"""
+
+__author__ = 'Patrick Barone'
+__email__ = 'barone.patrick@gmail.com'
+
 import cv2
+import os
 import glob
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-image_path = './camera_cal'
-show_images = False
+### User configuration parameters ###
+input_image_path = './camera_cal'
+annotated_image_path = './output_camera_calibration'
+annotated_image_prefix = 'annotated_'
+#####################################
 
-def process_image(img, w, h):
+def process_image(path, w, h):
+    '''
+    Process image at given path and return objectpoints, cornrers and save annotated image
+    '''
+
+    img = cv2.imread(path)
+    img_name = os.path.basename(path)
     objp = np.zeros((w*h, 3), np.float32)
     objp[:,:2] = np.mgrid[0:w, 0:h].T.reshape(-1,2)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     ret, corners = cv2.findChessboardCorners(gray, (w,h), None)
-    if show_images:
-        annotated_img = cv2.drawChessboardCorners(img, (9, 6), corners, ret)
-        plt.imshow(img)
-        plt.show(block=True)
+    annotated_img = cv2.drawChessboardCorners(img, (9, 6), corners, ret)
+
+    output_path = os.path.join(annotated_image_path, annotated_image_prefix + img_name)
+    cv2.imwrite(output_path, annotated_img)
+
+    # Unable to identify corners in image
     if not ret:
         return None, None
+
     return objp, corners
 
 if __name__ == '__main__':
-    print('Startup')
 
-    paths = glob.glob(image_path + '/*.jpg')
+    # Generate list of all images in the folder
+    paths = glob.glob(input_image_path + '/*.jpg')
     n_images = len(paths)
 
     objpoints = []
     imgpoints = []
 
     for path in paths:
-        img = cv2.imread(path)
-        objp, corners = process_image(img, 9, 6)
+        objp, corners = process_image(path, 9, 6)
 
         if objp is not None:
             print('Path: (%s) Status: (%s)' % (path,'SUCCESS')) 
@@ -41,7 +62,9 @@ if __name__ == '__main__':
             print('Path: (%s) Status: (%s)' % (path,'SKIPPED')) 
 
     
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[1::-1], None, None)
+    # Calculate camera calibration from the results of the camera calibrations
+    imshape = cv2.imread(paths[0]).shape
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, imshape[1::-1], None, None)
 
     # Save the camera calibration information
     calibration = {
@@ -51,5 +74,4 @@ if __name__ == '__main__':
             'rvecs': rvecs,
             'tvecs': tvecs
             }
-
     pickle.dump(calibration, open('camera_calibration.p', 'wb'))
